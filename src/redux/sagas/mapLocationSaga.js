cd1f8b5671666aa40d8cb651769e3cebecc5523a
Christopher O'Bannon <Crobwan@gmail.com>
2019-11-21 15:44:47 -0600

    creates a mapLocation reducer and Saga, initalizes them in their respective indexes

---
+++ src/redux/sagas/mapLocationSaga.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import {put, takeLatest} from 'redux-saga/effects';
+
+function * fetchMapLocation (action){
+    try{
+        const config = {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true,
+        };
+        const response = yield  axios.get('I NEED AN ADDRESs', config);
+        yield put({type: 'SET_MAP_LOCATION', payload: response.data});
+    }catch(error){
+    console.log('mapLocation get request failed', error);
+    }
+}
+
+
+function * mapLocationSaga(){
+    yield takeLatest('FETCH_MAP_LOCATION', fetchMapLocation)
+}
+
+export default mapLocationSaga
